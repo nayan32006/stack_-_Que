b561b4ededcdc21dcee000cfbb18cd55ce6d3cb2
nayan32006 <nayang389@gmail.com>
2026-04-20 10:24:23 +0530

    DAY 1 OF S&Q :: COMPLETE basicPUSH_POP_TOP_EMPTYelement

---
+++ basicPUSH_POP_TOP_EMPTYelement.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+#define n 100
+
+class stack {
+    int *arr;
+    int top;
+    public :
+    stack(){
+        arr = new int[n];
+        top = -1;
+    }
+    void push(int x){
+        if(top==n-1){
+            cout<<" Stack Overflow!"<<endl;
+            return;
+        }
+        top++;
+        arr [top]=x;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"no element in pop!"<<endl;
+            return;
+        }
+        top--;
+    }
+    int Top(){
+        if(top==-1){
+            cout<<"no element in stack!"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    bool empty(){
+        return -1;
+    }
+    
+};
+int main(){
+    stack st;
+    st.push(3);
+    st.push(2);
+    st.push(1);
+    st.push(0);
+    cout<<st.Top() <<endl;
+    st.pop();
+    cout<<st.Top() <<endl;
+    st.pop();
+    st.pop();
+    st.pop();
+    st.pop();
+    cout<<st.empty() <<endl;
+
+    return 0;
+}
